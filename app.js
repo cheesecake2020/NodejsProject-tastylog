73da9c235390586f729d55d98e9b7745b4168338
cheesecake2020 <well277balanced@gmail.com>
2021-10-19 16:59:20 +0900

    next commit

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 const path = require("path");
 const logger = require("./lib/log/logger.js");
 const accesslogger = require("./lib/log/accesslogger.js");
@@ -7,24 +7,33 @@ const express = require("express");
 const favicon = require("serve-favicon");
 const app = express();
 
-
-// Expressの設定
+// Express settings
 app.set("view engine", "ejs");
 app.disable("x-powered-by");
 
-// 静的コンテンツの配信の設定
+// Expose global method to view engine.
+app.use((req, res, next) => {
+  res.locals.moment = require("moment");
+  res.locals.padding = require("./lib/math/math.js").padding;
+  next();
+});
+
+// Static resource rooting.
 app.use(favicon(path.join(__dirname, "/public/favicon.ico")));
 app.use("/public", express.static(path.join(__dirname, "/public")));
 
-// アクセスロガー
+// Set access log.
 app.use(accesslogger());
-// 動的コンテンツの配信
+
+// Dynamic resource rooting.
+app.use("/search", require("./routes/search.js"));
+app.use("/shops", require("./routes/shops.js"));
 app.use("/", require("./routes/index.js"));
 
-// アプリケーションログの準備
+// Set application log.
 app.use(applicationlogger());
 
-// アプリケーションの実行
+// Execute web application.
 app.listen(PORT, () => {
-  logger.application.info(`Application listening at ${PORT}`);
+  logger.application.info(`Application listening at :${PORT}`);
 });
